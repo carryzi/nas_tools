7ba84e60572c60a6dc8ac669ec3855f7fb8e0d48
carryzi <61820543+carryzi@users.noreply.github.com>
2022-11-27 06:24:07 +0800

    Update naive.sh

--- NaiveProxy/naive.sh
+++ NaiveProxy/naive.sh
@@ -413,7 +413,7 @@ config() {
     mkdir /etc/caddy/
     mkdir /var/www/ -p
 
-    wget -c https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/html.tar.gz -O - | tar -xz -C /var/www/
+    wget -c https://raw.githubusercontent.com/carryzi/nas_tools/main/NaiveProxy/html.tar.gz -O - | tar -xz -C /var/www/
     # 生成密码
     # /etc/letsencrypt/live/x.dongvps.com/
 
@@ -699,7 +699,7 @@ show_cert(){
 }
 
 optimize(){
-    curl https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/optimize.sh | bash -
+    curl https://raw.githubusercontent.com/carryzi/nas_tools/main/NaiveProxy/optimize.sh | bash -
 }
 
 cert_renew(){
@@ -714,7 +714,7 @@ cert_renew(){
 }
 
 shell_renew(){
-    curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/naive.sh 
+    curl -o /root/.naive.sh https://raw.githubusercontent.com/carryzi/nas_tools/main/NaiveProxy/naive.sh 
     chmod +x /root/.naive.sh
     echo
     echo -e "$red naive更新完成，请重新执行naive $none"
